c4e49ccdf6795608eb5502fb6d946d353b6f27f0
Monk <stephanzych@gmail.com>
2012-05-10 17:19:18 +0300

    removing semicolon + async attribute

--- src/inject.js
+++ src/inject.js
@@ -1,7 +1,7 @@
 window.inject = inject = function(files, cb){
-	if(typeof files === 'string')	files = files.replace(/\s/g, '').split(',');
-	else if(files instanceof Array)	files = files;
-	else return false;
+	if(typeof files === 'string')	files = files.replace(/\s/g, '').split(',')
+	else if(files instanceof Array)	files = files
+	else return false
 	
 	var inject = this.inject
 	, file
@@ -9,27 +9,28 @@ window.inject = inject = function(files, cb){
 	, i = files.length
 	, head
 	, load = function(){
-		if(i === 0)	cb() || function(){};
+		if(i === 0)	cb() || function(){}
 	};
 	
 	if(/in/.test(document.readyState) || setTimeout('inject', 9)){
-		head = document.getElementsByTagName('head')[0];
+		head = document.getElementsByTagName('head')[0]
 		
 		while(file = files[--i]){
 			if(/\.css$/.test(file)){
-				node = document.createElement('link');
-				node.rel = 'stylesheet';
-				node.href = file;
+				node = document.createElement('link')
+				node.rel = 'stylesheet'
+				node.href = file
 			} else {
-				node = document.createElement('script');
-				node.src = file;
+				node = document.createElement('script')
+				node.async = true
+				node.src = file
 			}
 			node.onreadystatechange = function(){
-				if(this.readyState == 'complete')	load();
+				if(this.readyState == 'complete')	load()
 			};
-			node.onload = load();
-			head.appendChild(node);
+			node.onload = load()
+			head.appendChild(node)
 		}
 	}
-}; // window.inject, inject by Stéphan Zych (monkeymonk.be)
+} // window.inject, inject by Stéphan Zych (monkeymonk.be)
 // use: window.inject('test.js, test.css', function(){alert('all files are loaded!');});
