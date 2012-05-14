8f43270b403521c97945278c5c05deb1387d8cde
Zych Stéphan <stephanzych@gmail.com>
2012-05-14 16:52:45 +0200

    Cleaning

--- src/inject.js
+++ src/inject.js
@@ -2,15 +2,12 @@ window.inject = inject = function(files, cb){
 	if(typeof files === 'string')	files = files.replace(/\s/g, '').split(',');
 	else return false;
 	
-	var i = files.length
-		, file
-		, node
-		, head
-		, load = function(){
-			if(i === 0)	cb() || function(){};
-		};
+	var i = files.length, file, node, head
+	, load = function(){
+		if(i === 0)	cb() || function(){};
+	};
 	
-	if(/in/.test(document.readyState) || setTimeout('inject', 9)){
+	if(!/in/.test(document.readyState) || !!setTimeout(function(){inject(files, cb);}, 9)){
 		head = document.getElementsByTagName('head')[0];
 		
 		while(file = files[--i]){
