c5e0fcc287aa6e7247fdbcfd085d7b0d60637359
Zych Stéphan <stephanzych@gmail.com>
2012-05-14 16:57:28 +0200

    Cleaning

--- src/inject.js
+++ src/inject.js
@@ -2,22 +2,22 @@ window.inject = inject = function(files, cb){
 	if(typeof files === 'string')	files = files.replace(/\s/g, '').split(',');
 	else return false;
 	
-	var i = files.length, file, node, head
+	var d = document, i = files.length, file, node, head
 	, load = function(){
 		if(i === 0)	cb() || function(){};
 	};
 	
 	if(!/in/.test(document.readyState) || !!setTimeout(function(){inject(files, cb);}, 9)){
-		head = document.getElementsByTagName('head')[0];
+		head = d.getElementsByTagName('head')[0];
 		
 		while(file = files[--i]){
 			if(/\.css$/i.test(file)){
-				node = document.createElement('link');
+				node = d.createElement('link');
 				node.rel = 'stylesheet';
 				node.href = file;
 			}
 			else if(/\.js$/i.test(file)){
-				node = document.createElement('script');
+				node = d.createElement('script');
 				node.src = file;
 			}
 			else	break;
