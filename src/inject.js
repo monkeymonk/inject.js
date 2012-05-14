aedf859c5dd8dab08109e39097447bf6be8781b5
Zych Stéphan <stephanzych@gmail.com>
2012-05-14 16:32:56 +0200

    Cleaning

--- src/inject.js
+++ src/inject.js
@@ -1,18 +1,4 @@
 window.inject = inject = function(files, cb){
-<<<<<<< HEAD
-	if(typeof files === 'string')	files = files.replace(/\s/g, '').split(',')
-	else if(files instanceof Array)	files = files
-	else return false
-	
-	var inject = this.inject
-	, file
-	, node
-	, i = files.length
-	, head
-	, load = function(){
-		if(i === 0)	cb() || function(){}
-	};
-=======
 	if(typeof files === 'string')	files = files.replace(/\s/g, '').split(',');
 	else return false;
 	
@@ -23,22 +9,11 @@ window.inject = inject = function(files, cb){
 		, load = function(){
 			if(i === 0)	cb() || function(){};
 		};
->>>>>>> Cleaning
 	
 	if(/in/.test(document.readyState) || setTimeout('inject', 9)){
-		head = document.getElementsByTagName('head')[0]
+		head = document.getElementsByTagName('head')[0];
 		
 		while(file = files[--i]){
-<<<<<<< HEAD
-			if(/\.css$/.test(file)){
-				node = document.createElement('link')
-				node.rel = 'stylesheet'
-				node.href = file
-			} else {
-				node = document.createElement('script')
-				node.async = true
-				node.src = file
-=======
 			if(/\.css$/i.test(file)){
 				node = document.createElement('link');
 				node.rel = 'stylesheet';
@@ -47,19 +22,14 @@ window.inject = inject = function(files, cb){
 			else if(/\.js$/i.test(file)){
 				node = document.createElement('script');
 				node.src = file;
->>>>>>> Cleaning
 			}
 			else	break;
 			node.onreadystatechange = function(){
-<<<<<<< HEAD
-				if(this.readyState == 'complete')	load()
-=======
 				if(this.readyState === 'complete')	load();
->>>>>>> Cleaning
 			};
-			node.onload = load()
-			head.appendChild(node)
+			node.onload = load();
+			head.appendChild(node);
 		}
 	}
-} // window.inject, inject by Stéphan Zych (monkeymonk.be)
+}; // window.inject, inject by Stéphan Zych (monkeymonk.be)
 // use: window.inject('test.js, test.css', function(){alert('all files are loaded!');});
