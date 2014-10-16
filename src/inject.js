7b3e55686c825e4d22ea49d054a5dc1c85a7c17c
Stéphan Zych <stephanzych@gmail.com>
2014-10-16 21:55:07 +0200

    cleanup

--- src/inject.js
+++ src/inject.js
@@ -1,35 +1,50 @@
-;(function(window, document, undefined){
-	var inject = inject = function(files, cb){
-		if(typeof files === 'string')	files = files.replace(/\s/g, '').split(',')
-		else return false
-		
-		var d = document, i = files.length, file, node, head
-		, load = function(){
-			if(i === 0)	cb.call(this) || function(){}
+/**
+ * inject() by Stéphan Zych (monkeymonk.be)
+ * @use inject('test.js, test.css', function(){alert('all files are loaded!')});
+ */
+
+(function (window, document, undefined) {
+	window.inject = function (files, cb){
+		if (typeof files !== 'string')	{
+			return false;
 		}
 		
-		if(!/in/.test(document.readyState) || !!setTimeout(function(){inject(files, cb)}, 9)){
-			head = d.getElementsByTagName('head')[0]
+		files = files.replace(/\s/g, '').split(',');
+		
+		var d = document, 
+		    i = files.length, 
+		    file, node, head, 
+		    load = function () {
+				if (i === 0) {
+					cb.call(this) || function () {}
+				}
+			};
+		
+		if (!/in/.test(document.readyState) || !!setTimeout(function () {inject(files, cb)}, 9)) {
+			head = d.getElementsByTagName('head')[0];
 			
-			while(file = files[--i]){
-				if(/\.css$/i.test(file)){
-					node = d.createElement('link')
-					node.rel = 'stylesheet'
-					node.href = file
+			while (file = files[--i]) {
+				if (/\.css$/i.test(file)) {
+					node = d.createElement('link');
+					node.rel = 'stylesheet';
+					node.href = file;
+				} else if (/\.js$/i.test(file)) {
+					node = d.createElement('script');
+					node.src = file;
+				} else {
+					break;
 				}
-				else if(/\.js$/i.test(file)){
-					node = d.createElement('script')
-					node.src = file
-				}
-				else	break
-				node.onreadystatechange = function(){
-					if(this.readyState === 'complete')	load.call(this)
+				
+				node.onreadystatechange = function () {
+					if (this.readyState === 'complete') {
+						load.call(this);
+					}
 				};
-				node.onload = load
+				
+				node.onload = load;
+				
 				head.appendChild(node);
 			}
 		}
-	}
-	window.inject = inject
-})(window, document) // inject() by Stéphan Zych (monkeymonk.be)
-// use: inject('test.js, test.css', function(){alert('all files are loaded!')})
+	};
+}) (window, document);
