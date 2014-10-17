60a7db16c602f2bb59437378b2fe87fb5fa3384a
Stéphan Zych <stephanzych@gmail.com>
2014-10-17 21:17:40 +0200

    Update inject.js

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,7 @@
  * @use inject('test.js, test.css', function(){alert('all files are loaded!')});
  */
 
-(function (window, document, undefined) {
+(function (window, document) {
 	window.inject = function (files, cb){
 		if (typeof files !== 'string')	{
 			return false;
@@ -11,25 +11,24 @@
 		
 		files = files.replace(/\s/g, '').split(',');
 		
-		var d = document, 
-		    i = files.length, 
+		var i = files.length, 
 		    file, node, head, 
 		    load = function () {
-				if (i === 0) {
+			    if (i === 0) {
 					cb.call(this) || function () {}
 				}
 			};
 		
 		if (!/in/.test(document.readyState) || !!setTimeout(function () {inject(files, cb)}, 9)) {
-			head = d.getElementsByTagName('head')[0];
+			head = document.getElementsByTagName('head')[0];
 			
 			while (file = files[--i]) {
 				if (/\.css$/i.test(file)) {
-					node = d.createElement('link');
+					node = document.createElement('link');
 					node.rel = 'stylesheet';
 					node.href = file;
 				} else if (/\.js$/i.test(file)) {
-					node = d.createElement('script');
+					node = document.createElement('script');
 					node.src = file;
 				} else {
 					break;
